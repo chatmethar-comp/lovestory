7a5c8bf840cd204829933daef3ded346c8e4a00f
Fristki <narubet.cc@gmail.com>
2024-10-24 19:13:33 +0700

    Add basic background music

--- app.vue
+++ app.vue
@@ -1,23 +1,27 @@
 <template>
-    <div @click="enterFullScreen">
-      <NuxtPage />
-    </div>
-  </template>
-  
-  <script setup>
-  const enterFullScreen = () => {
-    const element = document.documentElement;
-    if (element.requestFullscreen) {
-      element.requestFullscreen();
-    } else if (element.mozRequestFullScreen) { // Firefox
-      element.mozRequestFullScreen();
-    } else if (element.webkitRequestFullscreen) { // Chrome, Safari and Opera
-      element.webkitRequestFullscreen();
-    } else if (element.msRequestFullscreen) { // IE/Edge
-      element.msRequestFullscreen();
-    }
-  };
-  </script>
-  
-  <style scoped>
-  </style>
+  <div @click="enterFullScreen">
+    <!-- Add the BGMusic component here -->
+    <BGMusic />
+    <NuxtPage />
+  </div>
+</template>
+
+<script setup>
+import BGMusic from '~/components/BGMusic.vue'; // Import the BGMusic component
+
+const enterFullScreen = () => {
+  const element = document.documentElement;
+  if (element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if (element.mozRequestFullScreen) { // Firefox
+    element.mozRequestFullScreen();
+  } else if (element.webkitRequestFullscreen) { // Chrome, Safari and Opera
+    element.webkitRequestFullscreen();
+  } else if (element.msRequestFullscreen) { // IE/Edge
+    element.msRequestFullscreen();
+  }
+};
+</script>
+
+<style scoped>
+</style>
